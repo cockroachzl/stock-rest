6f37eadcca1a842a424f9ee20bbe5540858ce84f
Liang Zhang <liang.zhang.cs@gmail.com>
2015-07-21 09:41:34 -0700

    remove some unused methods

--- StockFetcher.js
+++ StockFetcher.js
@@ -17,93 +17,9 @@ var assert = require('assert');
 function StockFetcher(db) {
     this.db = db;
     this.symbols = [];
-
-
-    //var insertQuotes = function(collectionName, quotes) {
-    //    // Get the documents collection
-    //    var collection = db.collection(collectionName);
-    //    console.log("insert into " + collectionName);
-    //    // Insert some documents
-    //    collection.insert(quotes, function(err, result) {
-    //        assert.equal(err, null);
-    //    });
-    //}
-
-
-    //this.update = function() {
-    //    //get stock watch list
-    //    var that = this;
-    //    this.db.collection('stocks').findOne({}, function (e, result) {
-    //            if (e) throw e;
-    //            that.symbols = result.symbols;
-    //            //insert into intraday db every 5 seconds if the stock market is open
-    //            if(StockFetcher.isMarketOpen()) {
-    //                that.fetch(that.symbols, insertQuotes.bind(undefined, 'intraday_quotes'));
-    //                //that.fetch(that.symbols, insertIntradayQuotes);
-    //            }
-    //            //insert into daily db if stock market is close and today's close quote is not in the db
-    //            else {
-    //                var now = moment().utcOffset(-4);
-    //                //var date = [now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()].join('-');
-    //                if(now.hours() < 10) { //next early morning
-    //                    now.subtract(1, 'days');
-    //                }
-    //                var date = now.format('YYYY-MM-DD');
-    //                var population;
-    //                that.db.collection('population').findOne({'date':date}, function (e, result) {
-    //                    if (e) throw e;
-    //                    population = result;
-    //                    if (!population) {
-    //                        console.log('fetching and populating interday quotes');
-    //                        that.fetch(that.symbols, insertQuotes.bind(undefined, 'interday_quotes'));
-    //                        population = {'date': date};
-    //                        that.db.collection('population').insert(population, function(err, result) {
-    //                            assert.equal(err, null);
-    //                        });
-    //                    }
-    //                    else {
-    //                        console.log('already populated ' + population.date);
-    //                    }
-    //                });
-    //            }
-    //        }
-    //    );
-    //
-    //}
-    /**
-     * Main processing function for communicating with Yahoo Finance API
-     * @param symbols an array of stock symbols
-     * return a list of quotes.
-     */
-    this.fetch = function (symbols, callback) {
-        var query = encodeURIComponent('select * from yahoo.finance.quotes ' +
-        'where symbol in (\'' + symbols.join(',') + '\')');
-        var urlWithParams = this.BASE + '?' + 'q=' + query + '&format=json&diagnostics=true' + '&env=' +
-            encodeURIComponent('store://datatables.org/alltableswithkeys');
-        var completeUrl = urlWithParams + '&callback=';
-        http.get(completeUrl, function (res) {
-            console.log(completeUrl);
-            res.setEncoding('utf8');
-            var data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                var obj = JSON.parse(data);
-                console.log('Got ' + obj.query.count + ' stocks');
-                callback(obj.query.results.quote);
-            })
-
-        }).on('error', function (e) {
-            console.log("Got error: " + e.message);
-        });
-    };
-
 }
 
-StockFetcher.prototype.BASE = 'http://query.yahooapis.com/v1/public/yql';
+StockFetcher.BASE = 'http://query.yahooapis.com/v1/public/yql';
 
 StockFetcher.prototype.insertPromise = function (collectionName, quotes) {
     var that = this;
@@ -145,32 +61,30 @@ StockFetcher.prototype.updateUsingPromise = function (stocksCollectionName, intr
     if (StockFetcher.isMarketOpen()) {
         return stocksPromise.then(this.fetchPromise.bind(this)).then(this.insertPromise.bind(this, intradayQuotesCollectionname));
     }
-    else {
-        var now = moment().utcOffset(-4);
-        //var date = [now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()].join('-');
-        if (now.hours() < 10) { //next early morning
-            now.subtract(1, 'days');
-        }
-        var date = now.format('YYYY-MM-DD');
-        var populationPromise = this.findOnePromise(populationCollectionName, {'date': date});
-
-        return populationPromise.then(function (value) {
-            if (!value) {
-                console.log('fetching and populating interday quotes');
-                return stocksPromise
-                    .then(that.fetchPromise.bind(that))
-                    .then(that.insertPromise.bind(that, interdayQuotesCollectionName))
-                    .then(function (value) {
-                        var population = {'date': date};
-                        return that.insertPromise(populationCollectionName, population);
-                    })
-                    .catch(console.log);
-            }
-            else {
-                console.log('already populated ' + value.date);
-            }
-        })
+    var now = moment().utcOffset(-4);
+    //var date = [now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()].join('-');
+    if (now.hours() < 10) { //next early morning
+        now.subtract(1, 'days');
     }
+    var date = now.format('YYYY-MM-DD');
+    var populationPromise = this.findOnePromise(populationCollectionName, {'date': date});
+
+    return populationPromise.then(function (value) {
+        if (!value) {
+            console.log('fetching and populating interday quotes');
+            return stocksPromise
+                .then(that.fetchPromise.bind(that))
+                .then(that.insertPromise.bind(that, interdayQuotesCollectionName))
+                .then(function (value) {
+                    var population = {'date': date};
+                    return that.insertPromise(populationCollectionName, population);
+                })
+                .catch(console.log);
+        }
+        else {
+            console.log('already populated ' + value.date);
+        }
+    });
 }
 
 /**
@@ -181,7 +95,7 @@ StockFetcher.prototype.updateUsingPromise = function (stocksCollectionName, intr
 StockFetcher.prototype.fetchPromise = function (symbols) {
     var query = encodeURIComponent('select * from yahoo.finance.quotes ' +
     'where symbol in (\'' + symbols.join(',') + '\')');
-    var urlWithParams = this.BASE + '?' + 'q=' + query + '&format=json&diagnostics=true' + '&env=' +
+    var urlWithParams = StockFetcher.BASE + '?' + 'q=' + query + '&format=json&diagnostics=true' + '&env=' +
         encodeURIComponent('store://datatables.org/alltableswithkeys');
     var completeUrl = urlWithParams + '&callback=';
 
--- test/TestPromise.js
+++ test/TestPromise.js
@@ -5,3 +5,15 @@ var promise = new Promise(function(resolve, reject){
     console.log('resolving');
     resolve(100);
 });
+
+Promise.resolve().then(function(value){
+    console.log("got " + value);
+});
+
+Promise.resolve('foo').then(Promise.resolve('bar')).then(function (result) {
+    console.log(result);
+});
+
+Promise.resolve('foo').then(null).then(function (result) {
+    console.log(result);
+});
